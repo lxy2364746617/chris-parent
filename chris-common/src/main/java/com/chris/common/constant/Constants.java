06f0c0edb1829f1aa862476982f8fafc0aea2de1
chris <2364746617@qq.com>
2023-11-11 16:54:21 +0800

    init

---
+++ chris-common/src/main/java/com/chris/common/constant/Constants.java
@@ -0,0 +1,13 @@
+package com.chris.common.constant;
+
+public class Constants {
+
+    public static final String SEC_KILL_GOODS_PREFIX = "SecKillGoods_";
+
+    public static final String SEC_KILL_ORDER_KEY = "SecKillOrder";
+
+    public static final String SEC_KILL_USER_QUEUE_KEY = "SecKillUserQueue";
+
+    public static final String SEC_KILL_QUEUE_KEY = "UserQueueCount";
+
+}
